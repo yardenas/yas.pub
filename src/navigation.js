80d23f1bce7b2244b240c13f8ab669cb2642345b
Yarden <yarden.as@inf.ethz.ch>
2024-02-10 15:13:00 +0100

    Add spotify and some other things

--- src/navigation.js
+++ src/navigation.js
@@ -6,10 +6,19 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-    { ariaLabel: 'Google Scholar', icon: 'academicons:google-scholar', href: 'https://github.com/onwidget/astrowind' },
+    {
+      ariaLabel: 'Google Scholar',
+      icon: 'academicons:google-scholar',
+      href: 'https://scholar.google.com/citations?user=Z0qGI_kAAAAJ&hl=en',
+    },
+    {
+      ariaLabel: 'Spotify',
+      icon: 'tabler:brand-spotify',
+      href: 'https://open.spotify.com/user/2sw6f4s1ipwy7bqshxeoyrevn?si=0c242994bf654506',
+    },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200"> Yarden As</a> · All rights reserved.
+    Made by <a class="text-primary dark:text-gray-200"> Yarden As</a> · All rights reserved.
   `,
 };
