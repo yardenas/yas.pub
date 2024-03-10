f23239030348a77442a375b3d77c52952f59a698
Yarden <yarden.as@inf.ethz.ch>
2024-03-10 18:26:21 +0100

    Adding ITL post

--- src/navigation.js
+++ src/navigation.js
@@ -2,8 +2,8 @@ import { getBlogPermalink, getHomePermalink, getPermalink } from '~/utils/permal
 export const headerData = {
   links: [
     { text: 'Publications & Talks', href: getPermalink('/publications') },
-    { text: 'CV', href: '/pdf/yarden-as-2022.pdf' },
     { text: 'Blog', href: getBlogPermalink() },
+    { text: 'CV', href: '/pdf/yarden-as-2022.pdf' },
     // { text: 'Stream of consciousness', href: '#' },
   ],
   position: 'right',
